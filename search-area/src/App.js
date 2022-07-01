b7f2a699970d3db7b52976c6bcdf60b1914a2aa8
Eduardo Avelino <101156420+Ed-18@users.noreply.github.com>
2022-06-30 22:59:47 -0300

    Add files via upload

---
+++ search-area/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { SearchArea } from './pages/SearchArea/search';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <SearchArea/>
+    </div>
+  );
+}
+
+export default App;
